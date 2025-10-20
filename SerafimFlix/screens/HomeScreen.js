b185027a12a714b9e52a12baea351c4db7808abe
Serafimsz7 <juaumxt1107@gmail.com>
2025-10-20 11:39:25 -0300

    projeto novo pt2

--- SerafimFlix/screens/HomeScreen.js
+++ SerafimFlix/screens/HomeScreen.js
@@ -1,5 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text } from 'expo-status-bar'
 import React from 'react'
+import { View, Text, Platform, TouchOpacity, ScrollView } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { BarsBottomLeftIcon, MagnifygGlassIcon } from 'react-native-heroicons/'
+import { styles } from '../theme'
+
+const ios = Platform.os == 'ios';
 
 export default function HomeScreen() {
   return (
